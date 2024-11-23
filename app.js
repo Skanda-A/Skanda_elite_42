7931319217ffdac04078fd425a86f963be9524cb
Skanda2228 <skanda2228@gmail.com>
2024-11-23 15:39:28 +0530

    first

---
+++ app.js
@@ -0,0 +1,5 @@
+import { a } from "helper.js";
+
+const result = multiply(1, 2);
+
+console.log(result);
---
+++ my-app/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return  <div className="App">This is Lauda's react App </div>
+}
+
+export default App;
